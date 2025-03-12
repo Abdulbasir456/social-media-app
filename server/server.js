d7bd6f5777b9f6d6358cc6e39a13a16741ad831b
Abdulbasir456 <abdulbasirhasan@gmail.com>
2025-03-12 17:20:21 +0000

    feat: connect MongoDB Atlas and configure environment setup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
-const app = express();
+const connectDB = require('./config/db');
 
 require('dotenv').config();
+const app = express();
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
